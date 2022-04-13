077844f1daf5ece43e2ce65cd88511c453300d3d
othmane kahtal <contact@othmanekahtal.me>
2022-04-13 14:57:07 +0000

    Upgrade to react 18

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
-import Views from "./views/views";
+import React from 'react'
+import {createRoot} from 'react-dom/client'
+import './index.css'
+import reportWebVitals from './reportWebVitals'
+import {BrowserRouter} from 'react-router-dom'
+import Views from './views/views'
 
-ReactDOM.render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Views />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById("root")
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
